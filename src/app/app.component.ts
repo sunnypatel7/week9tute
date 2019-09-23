b3a35083c41080faac3df38dcf3a2bc138ab5d42
Sunny Patel <sunny33p@gmail.com>
2019-09-23 11:17:25 +1000

    test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = 'w8ang';
+  title = "week 9 angular";
+  name: string;
+  price: number;
+  unit: number;
+  date: Date;
+  data = [];
+  item = {
+    name: this.name,
+    price: this.price,
+    unit: this.unit,
+    date: this.date
+  };
+  newItem() {
+    console.log(this.item);
+    this.data.push({
+      name: this.item.name,
+      price: this.item.price,
+      unit: this.item.unit,
+      date: this.item.date
+    });
+  }
+
+  clearItems() {
+    this.data = [];
+  }
 }
